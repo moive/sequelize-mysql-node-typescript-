c47260982fd5e67f6b943c6e37f615eae1e5b600
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-03-29 23:18:01 -0500

    get all planets

--- src/controllers/planet.controller.ts
+++ src/controllers/planet.controller.ts
@@ -5,7 +5,7 @@ import { getPlanetsService } from '../services/solarsys.service';
 const getPlanets: RequestHandler = async (_req: Request, res: Response) => {
   try {
     const planets: Planet[] = await getPlanetsService();
-    return res.json({ planets });
+    return res.json(planets);
   } catch (error) {
     return res.status(500).json({ message: 'Something went wrong', error });
   }
